b5a8f1caf0260f3e6d06ec0509af797b34938c63
bern4u2 <Bernadette.Werntges@CPowerEnergyManagement.com>
2017-05-10 10:35:59 -0400

    fixing errors
    timestamp, rootdirectory total size (sum of file sizes, not size of
    actual directory), add comments, made variables (rootdir, newdir) user
    input

--- collect_metadata.py
+++ collect_metadata.py
@@ -12,17 +12,15 @@ put in timestamp start and end
 add a cache
 """
 
-rootdir = "S:\KSQ File Share\siteapps\Documents" #location to look through files
-newdir = "C:/Users/Bernadette.Werntges/OneDrive - CPower/test/" #location to copy found files to
-logfile = os.path.join(newdir,'logfile.csv') #creates a log file under the new directory
 fieldnames = ['Filename', 'Path', 'Created Time', 'Modified Time', 'Size', 'File Type', 'Owner', 'Owner Domain'] # headers for logfile csv and organizing data
 results = []
-file_count = 0
 
 
 def parse_dirs(rootdir, newdir): # walk through all folders, subfolders in the root directory, and collect metadata
-	start_time = now()
-	rootfile_size = os.path.getsize(rootdir)
+	start_time = datetime.now()
+	rootfile_size = 0
+	file_count = 0
+	logfile = os.path.join(newdir,'logfile.csv') #creates a log file under the new directory
 	for subdir, dirs, files in os.walk(rootdir):
 		for file in files:
 			file_count +=1
@@ -30,6 +28,7 @@ def parse_dirs(rootdir, newdir): # walk through all folders, subfolders in the r
 				try:
 					path = os.path.join(subdir, file) #full file path (just adds the root file to the file name)
 					[mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime] = os.stat(path)
+					rootfile_size+=size
 					sd = win32security.GetFileSecurity (path, win32security.OWNER_SECURITY_INFORMATION)
 					owner_sid = sd.GetSecurityDescriptorOwner()
 					ownername, domain, typ = win32security.LookupAccountSid (None, owner_sid)	
@@ -38,11 +37,14 @@ def parse_dirs(rootdir, newdir): # walk through all folders, subfolders in the r
 					mtimedate = datetime.fromtimestamp(mtime).strftime('%Y/%m/%d %H:%M:%S') # convert seconds to date-time
 					# create a dictionary of {fieldnames: metadata} for the file and add to results list
 					results.append(dict(zip(fieldnames, [file,path,ctimedate,mtimedate,size,filetype, ownername, domain])))
+
+					#catch the pesky pywintypes errors which are usually some version of 'network drive not available' wait 15 seconds and try again
 				except error as e:
 					print (e,file)
 					time.sleep(15)
 					continue
 
+					#catch the winerror which is also usually some version of 'network drive not available' wait 15 seconds and try again
 				except OSError as err:
 					print (err, file)
 					time.sleep(15)
@@ -53,12 +55,12 @@ def parse_dirs(rootdir, newdir): # walk through all folders, subfolders in the r
 	#create logfile, add results
 	with open(logfile, 'w', newline='') as csvfile:
 		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-		writer.writeheader()
+		writer.writeheader() # headers are the fieldnames
 		writer.writerows(results)
-	end_time = now()
-	total_time = end_time - start-time
-	print (rootdir, "took ", total_time.days, 'days', total_time.seconds, 'seconds', total_time.microseconds, 'microseconds to parse through', //
-		file_count, 'files, a total size of', rootfile_size/1000000, 'MB')
+	end_time = datetime.now()
+	total_time = end_time - start_time # elapsed time
+	print (rootdir, "took ", total_time.days, 'days', total_time.seconds, 'seconds', total_time.microseconds, 'microseconds to parse through', \
+		file_count, 'files, a total size of', rootfile_size*(2**(-20)), 'MB')
 
 def create_new_dir(newdir): #creates the new directory if it doesn't exist already
 	if not os.path.exists(newdir):
@@ -66,6 +68,9 @@ def create_new_dir(newdir): #creates the new directory if it doesn't exist alrea
 
 
 def main():
+	rootdir = input('Enter root directory to parse through: ') #location to look through files
+	newdir = input('Enter directory to copy files to or where to store logfile: ')
+
 	create_new_dir(newdir)
 	parse_dirs(rootdir, newdir)
 
